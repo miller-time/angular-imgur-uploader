1d4390d63a61d7029b37af1d5d3f8ddffc50b976
Russell <miller.time.baby@gmail.com>
2016-06-13 21:41:50 -0700

    initial commit - create package for imgur upload service

---
+++ src/imgur-uploader.js
@@ -0,0 +1,53 @@
+(function() {
+
+    'use strict';
+
+    var app = angular.module('imgurUploader', ['imgurUploader.util']);
+
+    app.provider('imgur', function ImgurProvider() {
+        var clientId;
+
+        this.$get = function($http, $log, $q, fileReaderUtil) {
+            if (clientId) {
+                $log.debug('[imgur] Using Client ID: ' + clientId);
+            } else {
+                $log.error('[imgur] No Client ID configured!');
+            }
+
+            var upload = function(image, title, fileType) {
+                $log.debug('[imgur] Uploading image (title: ' + title + ')');
+
+                return $http.post(
+                    'https://api.imgur.com/3/image',
+                    {
+                        image: image,
+                        title: title,
+                        type: fileType
+                    },
+                    {
+                        headers: {
+                            Authorization: 'Client-ID ' + clientId,
+                            Accept: 'application/json'
+                        }
+                    }
+                );
+            };
+
+            var uploadBase64 = function(image, title) {
+                return fileReaderUtil(image).then(function(base64imageData) {
+                    return upload(base64imageData, title, 'base64');
+                });
+            };
+
+            return {
+                upload: upload,
+                uploadBase64: uploadBase64
+            };
+        };
+
+        this.setClientId = function(imgurClientId) {
+            clientId = imgurClientId;
+        };
+    });
+
+})();
---
+++ src/util.js
@@ -0,0 +1,39 @@
+(function() {
+
+    'use strict';
+
+    var app = angular.module('imgurUploader.util', []);
+
+    var arrayBufferToBase64String = function(buffer) {
+        var binary = '';
+        var bytes = new Uint8Array(buffer);
+        for (var i = 0; i < bytes.byteLength; ++i) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return btoa(binary);
+    };
+
+    app.factory('fileReaderUtil', function($log, $q) {
+        return function(image) {
+            var deferred = $q.defer();
+
+            var reader = new FileReader();
+
+            reader.onload = function() {
+                var arrayBuffer = reader.result;
+                $log.debug('[imgur] converting file to base-64...');
+                var base64Str = arrayBufferToBase64String(arrayBuffer);
+                deferred.resolve(base64Str);
+            };
+
+            reader.onerror = function() {
+                deferred.reject();
+            };
+
+            $log.debug('[imgur] reading file...');
+            reader.readAsArrayBuffer(image);
+            return deferred.promise;
+        };
+    });
+
+})();
